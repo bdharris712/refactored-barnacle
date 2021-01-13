0e379239f9766d12729a1dab468285575736aaa4
B3gg4r <bdharris712@gmail.com>
2021-01-12 21:39:17 -0700

    Starting week 19

---
+++ 00-My Code/19-Week/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,5 @@
+// Create a variable below
+var name="Brian";
+// Console.log() your variable here.
+console.log(name);
+console.log("Hello World!")
---
+++ 00-My Code/19-Week/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,27 @@
+// Create the addNumbers function here.
+var numTwo=2;
+
+function addNumbers() {
+    var numThree=3;
+    var sumFive=numTwo+numThree;
+    var productSix=numTwo*numThree;
+    console.log(sumFive);
+    console.log(productSix);
+    alert(numTwo+' + '+numThree+' = '+sumFive);
+}
+addNumbers();
+
+// Create the stringConcat function here.
+function tacoCat() {
+    var strTaco='Taco';
+    var strCat='Cat';
+    var strTacoCat=strTaco+strCat;
+    console.log(strTacoCat);
+    
+    console.log(strTaco +'s are more delicious than ' + strCat + 's.');
+    alert(strTacoCat);
+    alert('Made you look');
+}
+tacoCat();
+
+console.log(numTwo);
